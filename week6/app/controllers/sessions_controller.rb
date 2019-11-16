13be707e3501600bc82e7f1ecec3c5e46ddc00cb
zovivo <tmczovivo@gmail.com>
2019-11-16 17:32:12 +0700

    bt-week6

---
+++ week6/app/controllers/sessions_controller.rb
@@ -0,0 +1,35 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    if params[:session].present?
+      #Login normal
+      user = User.find_by(email: params[:session][:email].downcase)
+      if user && user.authenticate(params[:session][:password])
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_to user
+      else
+        flash.now[:danger] = 'Invalid email/password combination'
+        render 'new'
+      end
+    else
+      #Login with facebook
+      begin
+        user = User.from_omniauth(request.env['omniauth.auth'])
+        log_in user
+        flash[:success] = "Welcome"
+      rescue
+        flash[:danger] = "Error"
+      end
+      redirect_to user
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
+  end
+end
